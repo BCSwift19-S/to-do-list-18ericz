5c49cdaf0a4ba8d2964441693d620aa37aaadf73
Eric Zhou <46908112+18ericz@users.noreply.github.com>
2019-02-11 14:52:08 -0500

    Add files via upload
    Original File broke down due to SIGBART? Undid changes using GitHub created new Version 5.4

---
+++ to-do-list-18ericz-f69249c37faadbacbf3ed968bb24c63a85c3364e/DetailViewController.swift
@@ -0,0 +1,39 @@
+ //
+//  DetailViewController.swift
+//  ToDoList
+//
+//  Created by 18ericz on 2/10/19.
+//  Copyright © 2019 18ericz. All rights reserved.
+//
+
+import UIKit
+ 
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    
+    var toDoItem: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let toDoItem = toDoItem {
+            toDoField.text = toDoItem
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UnwindFromSave" {
+            toDoItem = toDoField.text
+        }
+    }
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
